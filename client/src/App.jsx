618a94daa2c77d5009574226c9574aca16928eda
Himasha Herath <himasha626@gmail.com>
2023-01-25 11:20:53 +0530

    HeroSection -Update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,24 @@
 import {
-  NavBar
+  NavBar,
+  Hero,
+  QR,
+  Manufacturer,
+  Review,
+  AboutUS,
+  Footer,
 } from "./components/section";
 
 const App = () => (
   <div className="min-h-screen">
     <div className="gradient-bg-welcome">
       <NavBar />
+      <Hero />
     </div>
+    <QR />
+    <Manufacturer />
+    <Review />
+    <AboutUS />
+    <Footer />
   </div>
 );
 
