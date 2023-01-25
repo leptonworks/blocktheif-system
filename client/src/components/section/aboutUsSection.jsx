0f98fd6a0d659fbafed214444efd086a194e4605
Himasha Herath <himasha626@gmail.com>
2023-01-25 14:10:02 +0530

    component-update

--- client/src/components/section/aboutUsSection.jsx
+++ client/src/components/section/aboutUsSection.jsx
@@ -2,8 +2,22 @@ import React from 'react'
 
 function aboutUsSection() {
   return (
-    <div>aboutUsSection</div>
-  )
+    <div className="flex w-full justify-left items-center gradient-bg-services">
+      <div className="flex mf:flex-row flex-col items-center justify-between md:p-20 py-12 px-4">
+        <div className="flex-1 flex flex-col justify-start items-start">
+          <h1 className="text-white text-3xl sm:text-5xl py-2 text-gradient ">
+            About
+            <br />
+            Us
+          </h1>
+          <p className="text-left my-2 text-white font-light md:w-9/12 w-11/12 text-base">
+          "Learn who we are, what we stand for, and why we do what we do."
+          </p>
+        </div>
+
+        <div className="flex-1 flex flex-col justify-start items-center"></div>
+      </div>
+    </div>  )
 }
 
 export default aboutUsSection
--- client/src/components/section/manuSection.jsx
+++ client/src/components/section/manuSection.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
-
+import React from 'react';
 function manuSection() {
   return (
-    <div>manuSection</div>
-  )
+    <div className="flex w-full justify-left items-center gradient-bg-services">
+      <div className="flex mf:flex-row flex-col items-center justify-between md:p-20 py-12 px-4">
+        <div className="flex-1 flex flex-col justify-start items-start">
+          <h1 className="text-white text-3xl sm:text-5xl py-2 text-gradient ">
+            Are You A 
+            <br />
+            Manufacturer ?
+          </h1>
+          <p className="text-left my-2 text-white font-light md:w-9/12 w-11/12 text-base">
+          "Join the revolution in product authenticity. Become a registered manufacturer on BlockThief."
+          </p>
+        </div>
+ 
+        <div className="flex-1 flex flex-col justify-start items-center"></div>
+      </div>
+    </div>  )
 }
 
 export default manuSection
