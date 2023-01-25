24564fe05ffccf87414455450adb209b398a671d
Himasha Herath <himasha626@gmail.com>
2023-01-25 14:44:17 +0530

    Button-Update

--- client/src/components/buttons/learnMore.jsx
+++ client/src/components/buttons/learnMore.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { AiFillPlayCircle } from "react-icons/ai";
 
 function learnMore() {
+  const ScrollDown = () => {};
+
   return (
-    <div>learnMore</div>
+    <button
+    type="button"
+    onClick={ScrollDown}
+    className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
+  >
+    <AiFillPlayCircle className="text-white mr-2" />
+    <p className="text-white text-base font-semibold">Learn More</p>
+  </button>
   )
 }
 
--- client/src/components/section/heroSection.jsx
+++ client/src/components/section/heroSection.jsx
@@ -3,6 +3,9 @@ import { AiFillPlayCircle } from "react-icons/ai";
 import { SiEthereum } from "react-icons/si";
 import { BsInfoCircle } from "react-icons/bs";
 import { Loader } from "./";
+import LearnMore from "../buttons/learnMore";
+
+
 
 const companyCommonStyles =
   "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
@@ -19,7 +22,6 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
 );
 
 const Welcome = () => {
-  const connectWallet = () => {};
   const handleSubmit = () => {};
   return (
     <div className="flex w-full justify-center items-center">
@@ -34,14 +36,7 @@ const Welcome = () => {
             solution ensures product authenticity. Protect your brand, build
             consumer trust and secure your supply chain with us."
           </p>
-          <button
-            type="button"
-            onClick={connectWallet}
-            className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
-          >
-            <AiFillPlayCircle className="text-white mr-2" />
-            <p className="text-white text-base font-semibold">Connect Wallet</p>
-          </button>
+          <LearnMore />
           <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
             <div className={`rounded-tl-2xl ${companyCommonStyles}`}>
               Blockchain
