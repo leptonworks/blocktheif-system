1780bc461e8882a278918b0de367b39cb4ba2edf
Himasha Herath <himasha626@gmail.com>
2023-01-25 11:28:09 +0530

    Footer-update

--- client/src/components/section/footer.jsx
+++ client/src/components/section/footer.jsx
@@ -18,13 +18,13 @@ const footer = () => (
 
     <div className="flex justify-center items-center flex-col mt-5">
       <p className="text-white text-sm text-center">Discover the unexpected potential of security and authenticity with our blockchain-based product authentication system.</p>
-      <p className="text-white text-sm text-center font-medium mt-2">info@kryptomastery.com</p>
+      <p className="text-white text-sm text-center font-medium mt-2">leptonworks@gmail.com</p>
     </div>
 
     <div className="sm:w-[90%] w-full h-[0.25px] bg-gray-400 mt-5 " />
 
     <div className="sm:w-[90%] w-full flex justify-between items-center mt-3">
-      <p className="text-white text-left text-xs">@kryptomastery2022</p>
+      <p className="text-white text-left text-xs">@BlockThief2022</p>
       <p className="text-white text-right text-xs">All rights reserved</p>
     </div>
   </div>
