412b4ebc55977749c067a9e2b4fd202735424e0a
MINDIYA <114866147+MINDIYA@users.noreply.github.com>
2023-03-01 07:54:57 +0530

    Revert "Content add"

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
 # blocktheif-system
-
-Packages    -    Client   -  npm install 
-                            npm install react-icons --save
-                            npm i react-router-dom
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,49 +1,34 @@
-import {
-  Navbar,
-  Hero,
-  QR,
-  Manufacturer,
-  Review,
-  AboutUS,
-  Footer,
-  ScanQrPage,
-  ManufacturerPage,
-  AboutUsPage,
-  LoginPage,
-  ReviewPage,
-} from "./components/section";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import ScanQR from "./pages/user/scanQR";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
 
-const App = () => (
-  <BrowserRouter>
-    <div className="min-h-screen">
-      <div className="gradient-bg-welcome">
-        <Navbar />
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
-      <Routes>
-        <Route path="/scanQR" element={<ScanQrPage />} />
-        <Route path="/Manufacturer" element={<ManufacturerPage />} />
-        <Route path="/AboutUs" element={<AboutUsPage />} />
-        <Route path="/Login" element={<LoginPage />} />
-        <Route path="/Reviews" element={<ReviewPage />} />
-
-        <Route
-          path="/"
-          element={
-            <>
-              <Hero />
-              <QR />
-              <Manufacturer />
-              <Review />
-              <AboutUS />
-              <Footer />
-            </>
-          }
-        />
-      </Routes>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </div>
-  </BrowserRouter>
-);
+  )
+}
 
-export default App;
+export default App
