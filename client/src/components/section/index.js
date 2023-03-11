4154d268b28ab35338095fcacc3d5f96ef33ec08
thakshana02 <thakshana.20210441@iit.ac.lk>
2023-03-12 04:15:44 +0530

    login-updated

--- client/src/components/section/index.js
+++ client/src/components/section/index.js
@@ -7,11 +7,14 @@ export { default as Review } from "./reviewSection";
 export { default as AboutUS } from "./aboutUsSection";
 export { default as Manufacturer } from "./manuSection";
 export { default as Loader } from "./Loader";
+
 // PAGES
 export { default as ScanQrPage } from "../../pages/user/scanQR";
+export { default as WebCamPage } from "../../pages/user/webCam";
 export { default as ManufacturerPage } from "../../pages/manufacturer/manuacturer";
 export { default as AboutUsPage } from "../../pages/aboutUs";
 export { default as LoginPage } from "../../pages/login";
 export { default as ReviewPage } from "../../pages/review";
 export { default as AddProduct } from "../../pages/manufacturer/addProduct"
-// export { default as ManuDashboard } from "../../pages/manufacturer/manuDashboard"
+export { default as ManuDashboard } from "../../pages/manufacturer/manuDashboard"
+export { default as Manulogin } from "../../pages/manufacturer/manuLogin"
