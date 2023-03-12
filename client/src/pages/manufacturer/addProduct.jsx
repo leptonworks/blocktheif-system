ce79e30182af106d902ffe78556751bcf66f6c26
Himasha Herath <himasha626@gmail.com>
2023-03-12 14:31:07 +0530

    addProduct-update

--- client/src/pages/manufacturer/addProduct.jsx
+++ client/src/pages/manufacturer/addProduct.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { ethers } from "ethers";
 import greeter from "../../../../contract/artifacts/contracts/Greeter.sol/Product.json";
+import QRCode from "qrcode";
 
-function addProduct() {  
 
+function addProduct() {
   const [productName, setProductName] = useState("");
 
   const [productEmei, setProductEmei] = useState("");
@@ -12,13 +13,24 @@ function addProduct() {
 
   const [productPrice, setProductPrice] = useState(0);
   const [productId, setProductId] = useState(0);
-  const [product, setProduct] = useState({ name: "", emei:"", color: "",size:"", price: 0 });
+  const [product, setProduct] = useState({
+    name: "",
+    emei: "",
+    color: "",
+    size: "",
+    price: 0,
+  });
   const [successMessage, setSuccessMessage] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
 
+  const [qrValue, setQrValue] = useState("");
+  const [qrImgUrl, setQrImgUrl] = useState("");
+
   const contractAddress = "0x73511669fd4dE447feD18BB79bAFeAC93aB7F31f";
   const ABI = greeter.abi;
-  const provider = new ethers.providers.JsonRpcProvider("http://localhost:8545");
+  const provider = new ethers.providers.JsonRpcProvider(
+    "http://localhost:8545"
+  );
   const signer = provider.getSigner();
   const contract = new ethers.Contract(contractAddress, ABI, signer);
 
@@ -49,8 +61,20 @@ function addProduct() {
   const handleAddProductSubmit = async (e) => {
     e.preventDefault();
     console.log("Submitting add product form...");
+    const productId = new Date().getTime(); // Generate unique ID using timestamp
     try {
-      await contract.addProduct(productId, productName,productEmei,productColor,productSize,productPrice);
+      console.log(productId);
+      await contract.addProduct(
+        productId,
+        productName,
+        productEmei,
+        productColor,
+        productSize,
+        productPrice
+      );
+      const qr = await QRCode.toDataURL(productId.toString());
+      console.log(qr)
+      setQrImgUrl(qr);
       console.log("Product added successfully.");
       alert("Product added successfully!");
       setSuccessMessage("Product added successfully!");
@@ -59,7 +83,6 @@ function addProduct() {
       console.error(err);
       alert("Error: ID already exists. Please choose another ID");
       setErrorMessage("Error: ID already exists. Please choose another ID.");
-
     }
   };
 
@@ -67,8 +90,16 @@ function addProduct() {
     e.preventDefault();
     console.log("Submitting get product form...");
     try {
-      const [name, emei, color, size, price] = await contract.getProduct(productId);
-      setProduct({ name, emei, color, size, price: ethers.utils.formatEther(price) });
+      const [name, emei, color, size, price] = await contract.getProduct(
+        productId
+      );
+      setProduct({
+        name,
+        emei,
+        color,
+        size,
+        price: ethers.utils.formatEther(price),
+      });
       console.log("Product retrieved successfully.");
     } catch (err) {
       console.error(err);
@@ -100,122 +131,121 @@ function addProduct() {
               <p className="mt-2">Product Price: {product.price} ETH</p>
             </div>
           </div>
-          </div>
+        </div>
       </div>
 
-          <div className="col-span-1 w-full h-full text-center bg-[#1E1E1E] py-32 flex items-center justify-center">
-            <div className="flex w-full justify-center items-center">
-              <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
-                <div className="flex-1 flex-col md:mr-10">
-                  <h4 className="text-white text-lg font-bold mb-2">Add Product</h4>
-                  <form onSubmit={handleAddProductSubmit}>
-                    <div className="mb-3">
-                      <input
-                        type="number"
-                        className="w-full border-2 border-gray-300 p-2 rounded-md focus:outline-none focus:border-blue-500"
-                        placeholder="Product ID"
-                        onChange={handleProductIdChange}
-                        value={productId}
-                      />
-                    </div>
-                    <div className="mb-3">
-                      <input
-                        type="text"
-                        className="w-full border-2 border-gray-300 p-2 rounded-md focus:outline-none focus:border-blue-500"
-                        placeholder="Product Name"
-                        onChange={handleProductNameChange}
-                        value={productName}
-                      />
-                    </div>
-                    <div className="mb-3">
-                      <input
-                        type="text"
-                        className="w-full border-2 border-gray-300 p-2 rounded-md focus:outline-none focus:border-blue-500"
-                        placeholder="Product Emei"
-                        onChange={handleProductEmeiChange}
-                        value={productEmei}
-                      />
-                    </div>
-                    <div className="mb-3">
-                      <input
-                        type="text"
-                        className="w-full border-2 border-gray-300 p-2 rounded-md focus:outline-none focus:border-blue-500"
-                        placeholder="Product Color"
-                        onChange={handleProductColorChange}
-                        value={productColor}
-                      />
-                    </div>
-                    <div className="mb-3">
-                      <input
-                        type="text"
-                        className="w-full border-2 border-gray-300 p-2 rounded-md focus:outline-none focus:border-blue-500"
-                        placeholder="Product Size"
-                        onChange={handleProductSizeChange}
-                        value={productSize}
-                      />
-                    </div>
-                    <div className="mb-3">
-                      <input
-                        type="number"
-                        className="w-full border-2 border-gray-300 p-2 rounded-md focus:outline-none focus:border-blue-500"
-                        placeholder="Product Price"
-                        onChange={handleProductPriceChange}
-                        value={productPrice}
-                      />
-                    </div>
-                    <button
-                      type="submit"
-                      className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
-                    >
-                      Add Product
-                    </button>
-                    {successMessage && (
-                      <div className="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded mt-3">
-                        {successMessage}
-                      </div>
-                    )}
-                  </form>
-
-                  <h4 className="text-white text-lg font-bold mt-3 mb-2">Get Product</h4>
-                  <form onSubmit={handleGetProductSubmit}>
-                    <div className="mb-3">
-                      <input
-                        type="number"
-                        className="w-full border-2 border-gray-300 p-2 rounded-md focus:outline-none focus:border-blue-500"
-                        placeholder="Product ID"
-                        onChange={handleProductIdChange}
-                        value={productId}
-                      />
-                    </div>
-                    <button
-                      type="submit"
-                      className="bg-gray-800 hover:bg-gray-900 text-white font-bold py-2 px-4 rounded"
-                    >
-                      Get Product
-                    </button>
-                  </form>
-                  <h4 className="text-white text-lg font-bold mt-3 mb-2">Remove Product</h4>
-                  <form onSubmit={handleRemoveProductSubmit}>
-                    <div className="mb-3">
-                      <input
-                        type="number"
-                        className="w-full border-2 border-gray-300 p-2 rounded-md focus:outline-none focus:border-blue-500"
-                        placeholder="Product ID"
-                        onChange={handleProductIdChange}
-                        value={productId}
-                      />
-                    </div>
-                    <button
-                      type="submit"
-                      className="bg-gray-800 hover:bg-gray-900 text-white font-bold py-2 px-4 rounded"
-                    >
-                      Remove Product
-                    </button>
-                  </form>
+      <div className="col-span-1 w-full h-full text-center bg-[#1E1E1E] py-32 flex items-center justify-center">
+        <div className="flex w-full justify-center items-center">
+          <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+            <div className="flex-1 flex-col md:mr-10">
+              <h4 className="text-white text-lg font-bold mb-2">Add Product</h4>
+              <form onSubmit={handleAddProductSubmit}>
+                <div className="mb-3">
+                  <input
+                    type="text"
+                    className="w-full border-2 border-gray-300 p-2 rounded-md focus:outline-none focus:border-blue-500"
+                    placeholder="Product Name"
+                    onChange={handleProductNameChange}
+                    value={productName}
+                  />
                 </div>
-              </div>
+                <div className="mb-3">
+                  <input
+                    type="text"
+                    className="w-full border-2 border-gray-300 p-2 rounded-md focus:outline-none focus:border-blue-500"
+                    placeholder="Product Emei"
+                    onChange={handleProductEmeiChange}
+                    value={productEmei}
+                  />
+                </div>
+                <div className="mb-3">
+                  <input
+                    type="text"
+                    className="w-full border-2 border-gray-300 p-2 rounded-md focus:outline-none focus:border-blue-500"
+                    placeholder="Product Color"
+                    onChange={handleProductColorChange}
+                    value={productColor}
+                  />
+                </div>
+                <div className="mb-3">
+                  <input
+                    type="text"
+                    className="w-full border-2 border-gray-300 p-2 rounded-md focus:outline-none focus:border-blue-500"
+                    placeholder="Product Size"
+                    onChange={handleProductSizeChange}
+                    value={productSize}
+                  />
+                </div>
+                <div className="mb-3">
+                  <input
+                    type="number"
+                    className="w-full border-2 border-gray-300 p-2 rounded-md focus:outline-none focus:border-blue-500"
+                    placeholder="Product Price"
+                    onChange={handleProductPriceChange}
+                    value={productPrice}
+                  />
+                </div>
+                <button
+                  type="submit"
+                  className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+                >
+                  Add Product
+                </button>
+                {successMessage && (
+                  <div className="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded mt-3">
+                    {successMessage}
+                  </div>
+                )}
+              </form>
+              {qrImgUrl && (
+                <div>
+                  <img src={qrImgUrl} alt="QR code" />
+                </div>
+              )}
+              <h4 className="text-white text-lg font-bold mt-3 mb-2">
+                Get Product
+              </h4>
+              <form onSubmit={handleGetProductSubmit}>
+                <div className="mb-3">
+                  <input
+                    type="number"
+                    className="w-full border-2 border-gray-300 p-2 rounded-md focus:outline-none focus:border-blue-500"
+                    placeholder="Product ID"
+                    onChange={handleProductIdChange}
+                    value={productId}
+                  />
+                </div>
+                <button
+                  type="submit"
+                  className="bg-gray-800 hover:bg-gray-900 text-white font-bold py-2 px-4 rounded"
+                >
+                  Get Product
+                </button>
+              </form>
+              <h4 className="text-white text-lg font-bold mt-3 mb-2">
+                Remove Product
+              </h4>
+              <form onSubmit={handleRemoveProductSubmit}>
+                <div className="mb-3">
+                  <input
+                    type="number"
+                    className="w-full border-2 border-gray-300 p-2 rounded-md focus:outline-none focus:border-blue-500"
+                    placeholder="Product ID"
+                    onChange={handleProductIdChange}
+                    value={productId}
+                  />
+                </div>
+                <button
+                  type="submit"
+                  className="bg-gray-800 hover:bg-gray-900 text-white font-bold py-2 px-4 rounded"
+                >
+                  Remove Product
+                </button>
+              </form>
             </div>
           </div>
+        </div>
+      </div>
     </section>
   );
 }
--- client/src/pages/user/scanQR.jsx
+++ client/src/pages/user/scanQR.jsx
@@ -3,29 +3,23 @@ import React, { useState } from "react";
 import QRCode from "qrcode";
 
 function scanQR() {
-
-  const [qrValue,setQrValue] = useState("")
-  const [qrImgUrl,setQrImgUrl] = useState("")
-  
+  const [qrValue, setQrValue] = useState("");
+  const [qrImgUrl, setQrImgUrl] = useState("");
 
   //gg
 
-
-  const handleSubmit = async(e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if(!qrValue){
-      return alert("please enter some value")
+    if (!qrValue) {
+      return alert("please enter some value");
     }
 
     const response = await QRCode.toDataURL(qrValue);
+    console.log(qrValue)
     setQrImgUrl(response);
     setQrValue("");
-
-  }
-
-
-  
+  };
 
   return (
     <section className="h-screen grid grid-cols-1">
@@ -37,49 +31,45 @@ function scanQR() {
         </h1>
         <br />
 
-        <h3>
-          Create QR Code
-        </h3>
+        <h3>Create QR Code</h3>
 
         <div className="card-body">
-          <form onSubmit={handleSubmit}
-          className="d-flex align-itms-center justify-content-between">
-            <input 
-            placeholder="enter text"
-            type="text" 
-            className="form-control border-top-0 border-start-0 border-end-0"
-            style={{width : "40%"}} 
-            name="" 
-            id="text"
+          <form
+            onSubmit={handleSubmit}
+            className="d-flex align-itms-center justify-content-between"
+          >
+            <input
+              placeholder="enter text"
+              type="text"
+              className="form-control border-top-0 border-start-0 border-end-0"
+              style={{ width: "40%" }}
+              name=""
+              id="text"
               value={qrValue}
-              onChange = {(e) => setQrValue(e.target.value)}
-             />
-            <button 
-            type="submit"
-            className="btn btn-primary text-white"
-            style={{width : "7%"}}
-            > 
-            Generate
+              onChange={(e) => setQrValue(e.target.value)}
+            />
+            <button
+              type="submit"
+              className="btn btn-primary text-white"
+              style={{ width: "7%" }}
+            >
+              Generate
             </button>
           </form>
 
           {qrImgUrl && (
             <div className="mt-4 mx-auto">
-              <a href={qrImgUrl} download = "qr.png">
-              <img src={qrImgUrl} alt="QR code" />
+              <a href={qrImgUrl} download="qr.png">
+                <img src={qrImgUrl} alt="QR code" />
               </a>
-              </div>
+            </div>
           )}
-
         </div>
 
-        <br /><br /><br /><br />
-
-
-        
-
-
-
+        <br />
+        <br />
+        <br />
+        <br />
 
         <div className="flex items-center justify-center"></div>
       </div>
