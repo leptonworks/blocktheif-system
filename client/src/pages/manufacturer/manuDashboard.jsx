a4e5d3c5460735e35a709419d0b0d2167820e5ee
Himasha Herath <himasha626@gmail.com>
2023-03-14 22:34:14 +0530

    dashboard-update

--- client/src/pages/manufacturer/manuDashboard.jsx
+++ client/src/pages/manufacturer/manuDashboard.jsx
@@ -27,36 +27,95 @@ function ManuDashboard() {
       <nav className="flex flex-col items-center justify-between w-64 h-screen py-6 bg-gray-800 text-gray-400">
         <ul className="mt-32 flex flex-col items-center justify-center">
           <li
-            className={`px-4 py-5 hover:bg-gray-700 hover:text-white ${
+            className={`border-slate-700 inline-flex w-full items-center space-x-2 border-b bg-blue-800 px-2 py-3 transition duration-150 ease-linear hover:bg-white/5 ${
               activeMenuItem === "dashboard" ? "bg-gray-700 text-white" : ""
             }`}
             onClick={() => setActiveMenuItem("dashboard")}
           >
-            <a href="#">Overview</a>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="h-6 w-6 text-white"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3.75 6A2.25 2.25 0 016 3.75h2.25A2.25 2.25 0 0110.5 6v2.25a2.25 2.25 0 01-2.25 2.25H6a2.25 2.25 0 01-2.25-2.25V6zM3.75 15.75A2.25 2.25 0 016 13.5h2.25a2.25 2.25 0 012.25 2.25V18a2.25 2.25 0 01-2.25 2.25H6A2.25 2.25 0 013.75 18v-2.25zM13.5 6a2.25 2.25 0 012.25-2.25H18A2.25 2.25 0 0120.25 6v2.25A2.25 2.25 0 0118 10.5h-2.25a2.25 2.25 0 01-2.25-2.25V6zM13.5 15.75a2.25 2.25 0 012.25-2.25H18a2.25 2.25 0 012.25 2.25V18A2.25 2.25 0 0118 20.25h-2.25A2.25 2.25 0 0113.5 18v-2.25z"
+              />
+            </svg>
+            <div className="flex flex-col">
+              <span className="text-lg font-bold leading-5 text-white">
+                <a href="#">DashBoard</a>
+              </span>
+              <span className="hidden text-sm text-white/50 md:block">
+                Overview
+              </span>
+            </div>
           </li>
+
           <li
-            className={`px-4 py-5 hover:bg-gray-700 hover:text-white ${
+            className={`border-slate-700 inline-flex w-full items-center space-x-2 border-b px-2 py-3 transition duration-150 ease-linear hover:bg-white/5 ${
               activeMenuItem === "projects" ? "bg-gray-700 text-white" : ""
             }`}
             onClick={() => setActiveMenuItem("projects")}
           >
-            <a href="#">Product Management</a>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="h-6 w-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M2.25 7.125C2.25 6.504 2.754 6 3.375 6h6c.621 0 1.125.504 1.125 1.125v3.75c0 .621-.504 1.125-1.125 1.125h-6a1.125 1.125 0 01-1.125-1.125v-3.75zM14.25 8.625c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v8.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 01-1.125-1.125v-8.25zM3.75 16.125c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v2.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 01-1.125-1.125v-2.25z"
+              />
+            </svg>
+
+            <div className="flex flex-col">
+              <span className="text-lg font-bold leading-5 text-white">
+                <a href="#">Manage</a>
+              </span>
+              <span className="hidden text-sm text-white/50 md:block">
+                Product Management
+              </span>
+            </div>
           </li>
           <li
-            className={`px-4 py-5 hover:bg-gray-700 hover:text-white ${
+            className={`border-slate-700 inline-flex w-full items-center space-x-2 border-b px-2 py-3 transition duration-150 ease-linear hover:bg-white/5 ${
               activeMenuItem === "calendar" ? "bg-gray-700 text-white" : ""
             }`}
             onClick={() => setActiveMenuItem("calendar")}
           >
-            <a href="#">Analytics</a>
+            <div className="flex flex-col">
+              <span className="text-lg font-bold leading-5 text-white">
+                <a href="#">Analytics</a>
+              </span>
+              <span className="hidden text-sm text-white/50 md:block">
+                Overview
+              </span>
+            </div>{" "}
           </li>
+
           <li
-            className={`px-4 py-5 hover:bg-gray-700 hover:text-white ${
+            className={`border-slate-700 inline-flex w-full items-center space-x-2 border-b px-2 py-3 transition duration-150 ease-linear hover:bg-white/5 ${
               activeMenuItem === "settings" ? "bg-gray-700 text-white" : ""
             }`}
             onClick={() => setActiveMenuItem("settings")}
           >
-            <a href="#">Profile</a>
+            <div className="flex flex-col">
+              <span className="text-lg font-bold leading-5 text-white">
+                <a href="#">Profile</a>
+              </span>
+              <span className="hidden text-sm text-white/50 md:block">
+                Overview
+              </span>
+            </div>{" "}
           </li>
         </ul>
         <div className="flex flex-col items-center">
