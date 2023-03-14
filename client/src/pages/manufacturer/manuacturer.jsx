9805f8872e6b681dc56433713177fe23a7be6799
Kavindu Pramod <95103102+KavinduPramod@users.noreply.github.com>
2023-03-14 06:44:31 +0530

    Front end to DB

--- client/src/pages/manufacturer/manuacturer.jsx
+++ client/src/pages/manufacturer/manuacturer.jsx
@@ -1,34 +1,152 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 
-function manuacturer() {
+function Manufacturer() {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    manufacturer: "",
+    licensesNumber: "",
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    createManufacturer(formData);
+  };
+
+  const submitFormData = async () => {
+  try {
+    const response = await axios.post(
+      "http://localhost:1337/manufacturer-registration",
+      formData
+    );
+    console.log(response.data);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+
   return (
     <section className="h-screen grid md:grid-cols-2">
-      <div className="bg-gray-300 flex items-center justify-center ">
-        <h1 className="text-3xl sm:text-5xl  py-1">
-          Manufacturer <br /> Secure Your Products
-        </h1>
+      <div className="bg-gray-300 flex items-center justify-center">
+        <form onSubmit={handleSubmit} className="w-full md:w-4/5 lg:w-3/4">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4">
+            <div className="mb-4">
+              <input
+                type="text"
+                id="firstName"
+                name="firstName"
+                value={formData.firstName}
+                onChange={handleInputChange}
+                className="appearance-none border rounded w-full py-2 px-3 bg-gray-200 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                placeholder="First Name"
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <input
+                type="text"
+                id="lastName"
+                name="lastName"
+                value={formData.lastName}
+                onChange={handleInputChange}
+                className="appearance-none border rounded w-full py-2 px-3 bg-gray-200 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                placeholder="Last Name"
+                required
+              />
+            </div>
+            <div className="mb-4 col-span-2">
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={formData.email}
+                onChange={handleInputChange}
+                className="appearance-none border rounded w-full py-2 px-3 bg-gray-200 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                placeholder="Email"
+                required
+              />
+            </div>
+            <div className="mb-4 col-span-2">
+              <input
+                type="password"
+                id="password"
+                name="password"
+                value={formData.password}
+                onChange={handleInputChange}
+                className="appearance-none border rounded w-full py-2 px-3 bg-gray-200 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                placeholder="Password"
+                required
+              />
+            </div>
+            <div className="mb-4 col-span-2">
+              <input
+                type="password"
+                id="confirmPassword"
+                name="confirmPassword"
+                value={formData.confirmPassword}
+                onChange={handleInputChange}
+                className="appearance-none border rounded w-full py-2 px-3 bg-gray-200 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                placeholder="Confirm Password"
+                required
+              />
+            </div>
+            <div className="mb-4 col-span-2">
+              <select
+                id="manufacturer"
+                name="manufacturer"
+                value={formData.manufacturer}
+                onChange={handleInputChange}
+                className="appearance-none border rounded w-full py-2 px-3 bg-gray-200 text
+                gray-700 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                required
+              >
+                <option value="">Select Manufacturer</option>
+                <option value="Apple">Apple</option>
+                <option value="Samsung">Samsung</option>
+                <option value="Huawei">Huawei</option>
+              </select>
+            </div>
+            <div className="mb-4 col-span-2">
+              <input
+                type="text"
+                id="licensesNumber"
+                name="licensesNumber"
+                value={formData.licensesNumber}
+                onChange={handleInputChange}
+                className="appearance-none border rounded w-full py-2 px-3 bg-gray-200 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                placeholder="Licence Number"
+                required
+              />
+            </div>
+            <div className="mb-4 col-span-2 text-center">
+              <button
+                type="submit"
+                className="bg-gray-800 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              >
+                Register
+              </button>
+            </div>
+          </div>
+        </form>
       </div>
       <div className="col-span-1 w-full h-full text-center bg-[#1E1E1E] py-32 flex items-center justify-center">
         <div className="flex w-full justify-center items-center">
           <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
-            <div className="flex-1 flex-col md:mr-10">
-              <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
-                Block Thief <br /> Secure Your Products
-              </h1>
-              <div className="flex items-center justify-center">
-                <p className="mt-5 text-white font-light md:w-9/12 w-full text-base">
-                  If you are a certified manufacturer you can add your product
-                  to Block Thief. make sure your product is only one in the
-                  world with QR code generated by blockchain system.
-                </p>
-              </div>
-              <div className="flex items-center justify-center">
-
-                
-
-              </div>
-            </div>
+            {/* login */}
           </div>
         </div>
       </div>
@@ -36,4 +154,4 @@ function manuacturer() {
   );
 }
 
-export default manuacturer;
+export default Manufacturer;
---
+++ database/my-project/src/api/manufacture/controllers/manufacture.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * manufacture controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::manufacture.manufacture');
