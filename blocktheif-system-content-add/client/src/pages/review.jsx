e94e74fb9e4489773590647ea26995054ada7268
MINDIYA <mindiyamaitipe@gmail.com>
2023-03-26 17:17:59 +0530

    ui-change

--- blocktheif-system-content-add/client/src/pages/review.jsx
+++ blocktheif-system-content-add/client/src/pages/review.jsx
@@ -2,37 +2,283 @@ import React from "react";
 
 function review() {
   return (
-    <div className="flex w-full justify-left items-center bg-black">
-      <div className="flex mf:flex-row flex-col items-center justify-between md:p-20 py-12 px-4">
-        <div className="flex-1 flex flex-col justify-start items-start">
-          <h1 className="text-white text-3xl sm:text-5xl py-14 text-gradient ">
-            Customer
-            <br />
-            Reviews
-          </h1>
-          <p className="text-left my-2 text-white font-light md:w-9/12 w-11/12 text-base">
-            "Discover the unmatched satisfaction of our customers through their
-            candid reviews."
-          </p>
-          <p className="text-left my-2 text-white font-light md:w-9/12 w-11/12 text-base">
-            "Discover the unmatched satisfaction of our customers through their
-            candid reviews."
-          </p>
-          <p className="text-left my-2 text-white font-light md:w-9/12 w-11/12 text-base">
-            "Discover the unmatched satisfaction of our customers through their
-            candid reviews."
-          </p>
-          <p className="text-left my-2 text-white font-light md:w-9/12 w-11/12 text-base">
-            "Discover the unmatched satisfaction of our customers through their
-            candid reviews."
-          </p>
-          <div className = "flex items-center justify-center">
-
-              </div>
-
+    <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <div className="grid gap-5 lg:grid-cols-3 sm:max-w-sm sm:mx-auto lg:max-w-full">
+        <div className="overflow-hidden transition-shadow duration-300 bg-white rounded">
+          <a href="/" aria-label="Article">
+            <img
+              src="https://images.pexels.com/photos/932638/pexels-photo-932638.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260"
+              className="object-cover w-full h-64 rounded"
+              alt=""
+            />
+          </a>
+          <div className="py-5">
+            <p className="mb-2 text-xs font-semibold text-gray-600 uppercase">
+              13 Jul 2020
+            </p>
+            <a
+              href="/"
+              aria-label="Article"
+              className="inline-block mb-3 text-black transition-colors duration-200 hover:text-deep-purple-accent-700"
+            >
+              <p className="text-2xl font-bold leading-5">Diving to the deep</p>
+            </a>
+            <p className="mb-4 text-gray-700">
+              Sed ut perspiciatis unde omnis iste natus error sit sed quia
+              consequuntur magni voluptatem doloremque.
+            </p>
+            <div className="flex space-x-4">
+              <a
+                href="/"
+                aria-label="Likes"
+                className="flex items-start text-gray-800 transition-colors duration-200 hover:text-deep-purple-accent-700 group"
+              >
+                <div className="mr-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    className="w-5 h-5 text-gray-600 transition-colors duration-200 group-hover:text-deep-purple-accent-700"
+                  >
+                    <polyline
+                      points="6 23 1 23 1 12 6 12"
+                      fill="none"
+                      strokeMiterlimit="10"
+                    />
+                    <path
+                      d="M6,12,9,1H9a3,3,0,0,1,3,3v6h7.5a3,3,0,0,1,2.965,3.456l-1.077,7A3,3,0,0,1,18.426,23H6Z"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeMiterlimit="10"
+                    />
+                  </svg>
+                </div>
+                <p className="font-semibold">7.4K</p>
+              </a>
+              <a
+                href="/"
+                aria-label="Comments"
+                className="flex items-start text-gray-800 transition-colors duration-200 hover:text-deep-purple-accent-700 group"
+              >
+                <div className="mr-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    stroke="currentColor"
+                    className="w-5 h-5 text-gray-600 transition-colors duration-200 group-hover:text-deep-purple-accent-700"
+                  >
+                    <polyline
+                      points="23 5 23 18 19 18 19 22 13 18 12 18"
+                      fill="none"
+                      strokeMiterlimit="10"
+                    />
+                    <polygon
+                      points="19 2 1 2 1 14 5 14 5 19 12 14 19 14 19 2"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeMiterlimit="10"
+                    />
+                  </svg>
+                </div>
+                <p className="font-semibold">81</p>
+              </a>
+            </div>
+          </div>
         </div>
-
-        <div className="flex-1 flex flex-col justify-start items-center"></div>
+        <div className="overflow-hidden transition-shadow duration-300 bg-white rounded">
+          <a href="/" aria-label="Article">
+            <img
+              src="https://images.pexels.com/photos/1576937/pexels-photo-1576937.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;w=500"
+              className="object-cover w-full h-64 rounded"
+              alt=""
+            />
+          </a>
+          <div className="py-5">
+            <p className="mb-2 text-xs font-semibold text-gray-600 uppercase">
+              4 Nov 2020
+            </p>
+            <a
+              href="/"
+              aria-label="Article"
+              className="inline-block mb-3 text-black transition-colors duration-200 hover:text-deep-purple-accent-700"
+            >
+              <p className="text-2xl font-bold leading-5">Conquer the World</p>
+            </a>
+            <p className="mb-4 text-gray-700">
+              Sed ut perspiciatis unde omnis iste natus error sit sed quia
+              consequuntur magni voluptatem doloremque.
+            </p>
+            <div className="flex space-x-4">
+              <a
+                href="/"
+                aria-label="Likes"
+                className="flex items-start text-gray-800 transition-colors duration-200 hover:text-deep-purple-accent-700 group"
+              >
+                <div className="mr-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    className="w-5 h-5 text-gray-600 transition-colors duration-200 group-hover:text-deep-purple-accent-700"
+                  >
+                    <polyline
+                      points="6 23 1 23 1 12 6 12"
+                      fill="none"
+                      strokeMiterlimit="10"
+                    />
+                    <path
+                      d="M6,12,9,1H9a3,3,0,0,1,3,3v6h7.5a3,3,0,0,1,2.965,3.456l-1.077,7A3,3,0,0,1,18.426,23H6Z"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeMiterlimit="10"
+                    />
+                  </svg>
+                </div>
+                <p className="font-semibold">7.4K</p>
+              </a>
+              <a
+                href="/"
+                aria-label="Comments"
+                className="flex items-start text-gray-800 transition-colors duration-200 hover:text-deep-purple-accent-700 group"
+              >
+                <div className="mr-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    stroke="currentColor"
+                    className="w-5 h-5 text-gray-600 transition-colors duration-200 group-hover:text-deep-purple-accent-700"
+                  >
+                    <polyline
+                      points="23 5 23 18 19 18 19 22 13 18 12 18"
+                      fill="none"
+                      strokeMiterlimit="10"
+                    />
+                    <polygon
+                      points="19 2 1 2 1 14 5 14 5 19 12 14 19 14 19 2"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeMiterlimit="10"
+                    />
+                  </svg>
+                </div>
+                <p className="font-semibold">81</p>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="overflow-hidden transition-shadow duration-300 bg-white rounded">
+          <a href="/" aria-label="Article">
+            <img
+              src="https://images.pexels.com/photos/2123755/pexels-photo-2123755.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+              className="object-cover w-full h-64 rounded"
+              alt=""
+            />
+          </a>
+          <div className="py-5">
+            <p className="mb-2 text-xs font-semibold text-gray-600 uppercase">
+              28 Dec 2020
+            </p>
+            <a
+              href="/"
+              aria-label="Article"
+              className="inline-block mb-3 text-black transition-colors duration-200 hover:text-deep-purple-accent-700"
+            >
+              <p className="text-2xl font-bold leading-5">
+                Explore the beautiful
+              </p>
+            </a>
+            <p className="mb-4 text-gray-700">
+              Sed ut perspiciatis unde omnis iste natus error sit sed quia
+              consequuntur magni voluptatem doloremque.
+            </p>
+            <div className="flex space-x-4">
+              <a
+                href="/"
+                aria-label="Likes"
+                className="flex items-start text-gray-800 transition-colors duration-200 hover:text-deep-purple-accent-700 group"
+              >
+                <div className="mr-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    className="w-5 h-5 text-gray-600 transition-colors duration-200 group-hover:text-deep-purple-accent-700"
+                  >
+                    <polyline
+                      points="6 23 1 23 1 12 6 12"
+                      fill="none"
+                      strokeMiterlimit="10"
+                    />
+                    <path
+                      d="M6,12,9,1H9a3,3,0,0,1,3,3v6h7.5a3,3,0,0,1,2.965,3.456l-1.077,7A3,3,0,0,1,18.426,23H6Z"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeMiterlimit="10"
+                    />
+                  </svg>
+                </div>
+                <p className="font-semibold">7.4K</p>
+              </a>
+              <a
+                href="/"
+                aria-label="Comments"
+                className="flex items-start text-gray-800 transition-colors duration-200 hover:text-deep-purple-accent-700 group"
+              >
+                <div className="mr-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    stroke="currentColor"
+                    className="w-5 h-5 text-gray-600 transition-colors duration-200 group-hover:text-deep-purple-accent-700"
+                  >
+                    <polyline
+                      points="23 5 23 18 19 18 19 22 13 18 12 18"
+                      fill="none"
+                      strokeMiterlimit="10"
+                    />
+                    <polygon
+                      points="19 2 1 2 1 14 5 14 5 19 12 14 19 14 19 2"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeMiterlimit="10"
+                    />
+                  </svg>
+                </div>
+                <p className="font-semibold">81</p>
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
