3d3c647083eeada555372bfa40c859c63bb52f69
MINDIYA <mindiyamaitipe@gmail.com>
2023-03-27 06:25:56 +0530

    ui change(review)

--- client/src/pages/review.jsx
+++ client/src/pages/review.jsx
@@ -2,37 +2,160 @@ import React from "react";
 
 function review() {
   return (
-    <div className="flex w-full justify-left items-center bg-black">
-      <div className="flex mf:flex-row flex-col items-center justify-between md:p-20 py-12 px-4">
-        <div className="flex-1 flex flex-col justify-start items-start">
-          <h1 className="text-white text-3xl sm:text-5xl py-14 text-gradient ">
-            Customer
-            <br />
-            Reviews
-          </h1>
-          <p className="text-left my-2 text-white font-light md:w-9/12 w-11/12 text-base">
-            "Discover the unmatched satisfaction of our customers through their
-            candid reviews."
-          </p>
-          <p className="text-left my-2 text-white font-light md:w-9/12 w-11/12 text-base">
-            "Discover the unmatched satisfaction of our customers through their
-            candid reviews."
-          </p>
-          <p className="text-left my-2 text-white font-light md:w-9/12 w-11/12 text-base">
-            "Discover the unmatched satisfaction of our customers through their
-            candid reviews."
-          </p>
-          <p className="text-left my-2 text-white font-light md:w-9/12 w-11/12 text-base">
-            "Discover the unmatched satisfaction of our customers through their
-            candid reviews."
-          </p>
-          <div className = "flex items-center justify-center">
-
-              </div>
-
+    <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <div className="grid gap-5 lg:grid-cols-3 sm:max-w-sm sm:mx-auto lg:max-w-full">
+        <div className="overflow-hidden transition-shadow duration-300 bg-white rounded">
+          <a href="https://www.samsung.com/us/smartphones/galaxy-s23-ultra/" aria-label="Article">
+            <img
+              src="https://image-us.samsung.com/us/smartphones/galaxy-s23-ultra/images/galaxy-s23-ultra-highlights-kv.jpg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260"
+              className="object-cover w-full h-64 rounded"
+              alt=""
+            />
+          </a>
+          <div className="py-5">
+            <p className="mb-2 text-xs font-semibold text-gray-600 uppercase">
+              13 Jul 2020
+            </p>
+            <a
+              href="/"
+              aria-label="Article"
+              className="inline-block mb-3 text-black transition-colors duration-200 hover:text-deep-purple-accent-700"
+            >
+              <p className="text-2xl font-bold leading-5">samsung galaxy s23 ultra</p>
+            </a>
+            <p className="mb-4 text-gray-700">
+              Sed ut perspiciatis unde omnis iste natus error sit sed quia
+              consequuntur magni voluptatem doloremque.
+            </p>
+          </div>
         </div>
-
-        <div className="flex-1 flex flex-col justify-start items-center"></div>
+        <div className="overflow-hidden transition-shadow duration-300 bg-white rounded">
+          <a href="/" aria-label="Article">
+            <img
+              src="https://www.apple.com/v/iphone-14-pro/e/images/meta/iphone-14-pro_overview__3dn6st99cpea_og.png?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;w=500"
+              className="object-cover w-full h-64 rounded"
+              alt=""
+            />
+          </a>
+          <div className="py-5">
+            <p className="mb-2 text-xs font-semibold text-gray-600 uppercase">
+              4 Nov 2020
+            </p>
+            <a
+              href="https://www.apple.com/iphone-14-pro/"
+              aria-label="Article"
+              className="inline-block mb-3 text-black transition-colors duration-200 hover:text-deep-purple-accent-700"
+            >
+              <p className="text-2xl font-bold leading-5">iphone 14 pro max</p>
+            </a>
+            <p className="mb-4 text-gray-700">
+              Sed ut perspiciatis unde omnis iste natus error sit sed quia
+              consequuntur magni voluptatem doloremque.
+            </p>
+          </div>
+        </div>
+        <div className="overflow-hidden transition-shadow duration-300 bg-white rounded">
+          <a href="https://www.mi.com/global/product/xiaomi-13/" aria-label="Article">
+            <img
+              src="https://techcrunch.com/wp-content/uploads/2023/02/XIaomi-13-Pro.jpg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+              className="object-cover w-full h-64 rounded"
+              alt=""
+            />
+          </a>
+          <div className="py-5">
+            <p className="mb-2 text-xs font-semibold text-gray-600 uppercase">
+              28 Dec 2020
+            </p>
+            <a
+              href="/"
+              aria-label="Article"
+              className="inline-block mb-3 text-black transition-colors duration-200 hover:text-deep-purple-accent-700"
+            >
+              <p className="text-2xl font-bold leading-5">
+              xiaomi 13
+              </p>
+            </a>
+            <p className="mb-4 text-gray-700">
+              Sed ut perspiciatis unde omnis iste natus error sit sed quia
+              consequuntur magni voluptatem doloremque.
+            </p>
+          </div>
+        </div>
+        <div className="overflow-hidden transition-shadow duration-300 bg-white rounded">
+          <a href="https://www.samsung.com/us/smartphones/galaxy-z-flip4/" aria-label="Article">
+            <img
+              src="https://image-us.samsung.com/us/smartphones/galaxy-z-flip4/v2/images/galaxy-z-flip4_highlights_kv.jpg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260"
+              className="object-cover w-full h-64 rounded"
+              alt=""
+            />
+          </a>
+          <div className="py-5">
+            <p className="mb-2 text-xs font-semibold text-gray-600 uppercase">
+              13 Jul 2020
+            </p>
+            <a
+              href="/"
+              aria-label="Article"
+              className="inline-block mb-3 text-black transition-colors duration-200 hover:text-deep-purple-accent-700"
+            >
+              <p className="text-2xl font-bold leading-5">Galaxy Z Flip4</p>
+            </a>
+            <p className="mb-4 text-gray-700">
+              Sed ut perspiciatis unde omnis iste natus error sit sed quia
+              consequuntur magni voluptatem doloremque.
+            </p>
+          </div>
+        </div>
+        <div className="overflow-hidden transition-shadow duration-300 bg-white rounded">
+          <a href="https://www.oneplus.com/us" aria-label="Article">
+            <img
+              src="https://www.oneplus.com/content/dam/oasis/page/2023/global/home/salami-share.jpg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260"
+              className="object-cover w-full h-64 rounded"
+              alt=""
+            />
+          </a>
+          <div className="py-5">
+            <p className="mb-2 text-xs font-semibold text-gray-600 uppercase">
+              13 Jul 2020
+            </p>
+            <a
+              href="/"
+              aria-label="Article"
+              className="inline-block mb-3 text-black transition-colors duration-200 hover:text-deep-purple-accent-700"
+            >
+              <p className="text-2xl font-bold leading-5">ONEPLUS 11</p>
+            </a>
+            <p className="mb-4 text-gray-700">
+              Sed ut perspiciatis unde omnis iste natus error sit sed quia
+              consequuntur magni voluptatem doloremque.
+            </p>
+          </div>
+        </div>
+        <div className="overflow-hidden transition-shadow duration-300 bg-white rounded">
+          <a href="https://store.google.com/in/product/pixel_7_pro?hl=en-GB" aria-label="Article">
+            <img
+              src="https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/csm_Google_Pixel_7_Pro_1664884451_0_0.jpg_c452b81928.jpg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260"
+              className="object-cover w-full h-64 rounded"
+              alt=""
+            />
+          </a>
+          <div className="py-5">
+            <p className="mb-2 text-xs font-semibold text-gray-600 uppercase">
+              13 Jul 2020
+            </p>
+            <a
+              href="/"
+              aria-label="Article"
+              className="inline-block mb-3 text-black transition-colors duration-200 hover:text-deep-purple-accent-700"
+            >
+              <p className="text-2xl font-bold leading-5">Pixel 7 Pro</p>
+            </a>
+            <p className="mb-4 text-gray-700">
+              Sed ut perspiciatis unde omnis iste natus error sit sed quia
+              consequuntur magni voluptatem doloremque.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
