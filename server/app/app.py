fb3bb119e72f3374532cd6ccac963381b170655b
Himasha Herath <himasha626@gmail.com>
2023-03-31 00:04:47 +0530

    ML_Model_To_Review- intergrated

--- server/app/app.py
+++ server/app/app.py
@@ -11,6 +11,8 @@ def get_sentiment():
     sentiment_result = sentiment(text)
     return jsonify({'sentiment': sentiment_result})
 
+
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=False, host="0.0.0.0", port=4000)
+
 
