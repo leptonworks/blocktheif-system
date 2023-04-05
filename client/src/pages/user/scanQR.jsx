5d3e27e4c0b5a6e74ffa9d95fe4dcf8260daf1e0
hasini-kumarasinghe <hasinikumarasinghe1@gmail.com>
2023-04-06 00:34:43 +0530

    ScanQR update

--- client/src/pages/user/scanQR.jsx
+++ client/src/pages/user/scanQR.jsx
@@ -1,100 +1,99 @@
-import React, { useState } from "react";
+import React from "react";
 
-import QRCode from "qrcode";
+import QRImage from '../../../images/default_qrcode.png';
 
-function scanQR() {
-  const [qrValue, setQrValue] = useState("");
-  const [qrImgUrl, setQrImgUrl] = useState("");
 
-  //gg
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    if (!qrValue) {
-      return alert("please enter some value");
-    }
-
-    const response = await QRCode.toDataURL(qrValue);
-    console.log(qrValue)
-    setQrImgUrl(response);
-    setQrValue("");
-  };  
+function ScanQR() {
 
   return (
+
     <section className="h-screen grid grid-cols-1">
-      <div className="col-span-1 text-left bg-[#1E1E1E] text-black font-bold py-32 md:p-20">
-        <h1 className="text-black text-3xl sm:text-5xl py-2 text-gradient ">
-          Scan Your
-          <br />
-          QR
-        </h1>
-        <br />
 
-        <h3>Create QR Code</h3>
+      <div className="col-span-1 text-left bg-black text-white font-bold py-32 md:p-20">
 
-        <div className="card-body">
-          <form
-            onSubmit={handleSubmit}
-            className="d-flex align-itms-center justify-content-between"
-          >
-            <input
-              placeholder="enter text"
-              type="text"
-              className="form-control border-top-0 border-start-0 border-end-0"
-              style={{ width: "40%" }}
-              name=""
-              id="text"
-              value={qrValue}
-              onChange={(e) => setQrValue(e.target.value)}
-            />
-            <button
-              type="submit"
-              className="btn btn-primary text-white"
-              style={{ width: "7%" }}
-            >
-              Generate
-            </button>
-          </form>
+        <div className="grid grid-cols-3 gap-8">
 
-          {qrImgUrl && (
-            <div className="mt-4 mx-auto">
-              <a href={qrImgUrl} download="qr.png">
-                <img src={qrImgUrl} alt="QR code" />
-              </a>
-            </div>
-          )}
+          <div className="col-span-1">
+
+            <br></br><img src= {QRImage} alt="QR Code" className="w-full" />
+
+          </div>
+
+          <div className="col-span-2">
+
+            <h1 className="text-white text-3xl sm:text-5xl py-2 text-gradient"><br></br>
+
+              Scan Your QR
+
+            </h1>
+
+            <p className="text-gray-300">
+
+              Welcome to our QR code scanner! Use this page to authenticate your product by scanning its unique QR code. Our system will verify if the product is genuine or counterfeit, giving you peace of mind and confidence in your purchase. Simply point your camera at the QR code on your product and we'll take care of the rest. Happy scanning!
+
+              </p>
+            <br></br>
+
+            {/* <h2 className="text-white text-2xl mb-4"><br></br>Learn More</h2> */}
+
+          <p className="text-gray-300">
+
+            Our platform offers a simple and efficient way to verify the authenticity of products by scanning their QR codes. The system checks if the product is genuine or fake, giving customers peace of mind and a hassle-free way to ensure they are buying the real thing. By uploading their QR code, customers can confirm the legitimacy of their product and avoid purchasing counterfeits.
+
+          </p>
+
+          <a href="/" className="inline-flex items-center font-semibold tracking-wider text-teal-accent-400 hover:text-teal-accent-700 mt-4">
+
+           join now
+
+            <svg className="inline-block w-3 ml-2" fill="currentColor" viewBox="0 0 12 12">
+
+              <path d="M9.707,5.293l-5-5A1,1,0,0,0,3.293,1.707L7.586,6,3.293,10.293a1,1,0,1,0,1.414,1.414l5-5A1,1,0,0,0,9.707,5.293Z" />
+
+            </svg>
+
+          </a> 
+
+          </div>
+
         </div>
 
-        <br />
-        <br />
-        <br />
-        <br />
 
-        <div className="flex items-center justify-center"></div>
+
+        {/* <div className="mt-12 bg-gray-900 bg-opacity-75 rounded-lg p-8"> */}
+
+          {/* <h2 className="text-white text-2xl mb-4">Learn More</h2>
+
+          <p className="text-gray-300">
+
+            Our platform offers a simple and efficient way to verify the authenticity of products by scanning their QR codes. The system checks if the product is genuine or fake, giving customers peace of mind and a hassle-free way to ensure they are buying the real thing. By uploading their QR code, customers can confirm the legitimacy of their product and avoid purchasing counterfeits.
+
+          </p> */}
+
+          {/* <a href="/" className="inline-flex items-center font-semibold tracking-wider text-teal-accent-400 hover:text-teal-accent-700 mt-4">
+
+            Learn more about our system
+
+            <svg className="inline-block w-3 ml-2" fill="currentColor" viewBox="0 0 12 12">
+
+              <path d="M9.707,5.293l-5-5A1,1,0,0,0,3.293,1.707L7.586,6,3.293,10.293a1,1,0,1,0,1.414,1.414l5-5A1,1,0,0,0,9.707,5.293Z" />
+
+            </svg>
+
+          </a> */}
+
+        {/* </div> */}
+
       </div>
-      <div className="col-  span-1 text-center bg-gray-300 text-white font-bold py-32">
-        <p className="text-end my-2 text-black font-bold md:w-9/12 w-11/12 text-base">
-          "Empower yourself with the assurance of authenticity. Scan your
-          products with Blockthief's QR code scanner."
-        </p>
-      </div>
+
     </section>
+
   );
+
 }
 
-<section className="h-screen grid md:grid-cols-2 justify-center items-center">
-  <div className="flex w-full justify-left items-center gradient-bg-services">
-    <div className="flex mf:flex-row flex-col items-center justify-between md:p-20 py-12 px-4">
-      <div className="flex-1 flex flex-col justify-start items-start">
-        <p className="text-left my-2 text-white font-light md:w-9/12 w-11/12 text-base">
-          "Empower yourself with the assurance of authenticity. Scan your
-          products with Blockthief's QR code scanner."
-        </p>
-      </div>
-      <div className="flex-1 flex flex-col justify-start items-center"></div>
-    </div>
-  </div>
-</section>;
 
-export default scanQR;
+
+export default ScanQR;
+
