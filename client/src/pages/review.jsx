53257ed27cb1330de7660b780287201bd3fd4003
MINDIYA <mindiyamaitipe@gmail.com>
2023-04-06 00:52:35 +0530

    Add-Photo

--- client/src/pages/review.jsx
+++ client/src/pages/review.jsx
@@ -78,80 +78,8 @@ function review() {
             </p>
           </div>
         </div>
-        <div className="overflow-hidden transition-shadow duration-300 bg-white rounded">
-          <a href="https://www.samsung.com/us/smartphones/galaxy-z-flip4/" aria-label="Article">
-            <img
-              src="https://image-us.samsung.com/us/smartphones/galaxy-z-flip4/v2/images/galaxy-z-flip4_highlights_kv.jpg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260"
-              className="object-cover w-full h-64 rounded"
-              alt=""
-            />
-          </a>
-          <div className="py-5">
-            <p className="mb-2 text-xs font-semibold text-gray-600 uppercase">
-              13 Jul 2020
-            </p>
-            <a
-              href="/"
-              aria-label="Article"
-              className="inline-block mb-3 text-black transition-colors duration-200 hover:text-deep-purple-accent-700"
-            >
-              <p className="text-2xl font-bold leading-5">Galaxy Z Flip4</p>
-            </a>
-            <p className="mb-4 text-gray-700">
-            The Galaxy Z Flip 4 has a lot of refinements, such as a slimmer, lighter hinge and flatter edges. The new Snapdragon 8 Plus Gen 1 processor ensures that the Flip 4 shines in every task. Battery life is much better. The cameras are good, especially the night mode, but not quite $999 level.
-            </p>
-          </div>
-        </div>
-        <div className="overflow-hidden transition-shadow duration-300 bg-white rounded">
-          <a href="https://www.oneplus.com/us" aria-label="Article">
-            <img
-              src="https://www.oneplus.com/content/dam/oasis/page/2023/global/home/salami-share.jpg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260"
-              className="object-cover w-full h-64 rounded"
-              alt=""
-            />
-          </a>
-          <div className="py-5">
-            <p className="mb-2 text-xs font-semibold text-gray-600 uppercase">
-              13 Jul 2020
-            </p>
-            <a
-              href="/"
-              aria-label="Article"
-              className="inline-block mb-3 text-black transition-colors duration-200 hover:text-deep-purple-accent-700"
-            >
-              <p className="text-2xl font-bold leading-5">ONEPLUS 11</p>
-            </a>
-            <p className="mb-4 text-gray-700">
-              Sed ut perspiciatis unde omnis iste natus error sit sed quia
-              consequuntur magni voluptatem doloremque.
-            </p>
-          </div>
-        </div>
-        <div className="overflow-hidden transition-shadow duration-300 bg-white rounded">
-          <a href="https://store.google.com/in/product/pixel_7_pro?hl=en-GB" aria-label="Article">
-            <img
-              src="https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/csm_Google_Pixel_7_Pro_1664884451_0_0.jpg_c452b81928.jpg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260"
-              className="object-cover w-full h-64 rounded"
-              alt=""
-            />
-          </a>
-          <div className="py-5">
-            <p className="mb-2 text-xs font-semibold text-gray-600 uppercase">
-              13 Jul 2020
-            </p>
-            <a
-              href="/"
-              aria-label="Article"
-              className="inline-block mb-3 text-black transition-colors duration-200 hover:text-deep-purple-accent-700"
-            >
-              <p className="text-2xl font-bold leading-5">Pixel 7 Pro</p>
-            </a>
-            <p className="mb-4 text-gray-700">
-              Sed ut perspiciatis unde omnis iste natus error sit sed quia
-              consequuntur magni voluptatem doloremque.
-            </p>
-          </div>
-        </div>
+        
+      
       </div>
     </div>
   );
