3018c7838c78dfe8a67e01db403d7f9ac817afed
thakshana02 <thakshana.20210441@iit.ac.lk>
2023-04-06 00:04:28 +0530

    minor-change

--- client/src/pages/manufacturer/manuacturer.jsx
+++ client/src/pages/manufacturer/manuacturer.jsx
@@ -174,7 +174,7 @@ function Manufacturer() {
       className="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
       type="submit"
     >
-      Login
+      Logins
     </button>
   </form>
 </div>
