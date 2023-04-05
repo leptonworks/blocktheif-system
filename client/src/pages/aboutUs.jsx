f43015c9fa40c4b3e60baa2552b5339ec6e5551f
MINDIYA <mindiyamaitipe@gmail.com>
2023-04-06 00:03:05 +0530

    ui-change

--- client/src/pages/aboutUs.jsx
+++ client/src/pages/aboutUs.jsx
@@ -4,72 +4,209 @@ import React from 'react'
 function aboutUs() {
   return (
     
-      <div class="bg-[#1E1E1E] bg-[url('/images/abdul-a-wmdfPWB2lWM-unsplash.jpg')] w-full h-full bg-cover bg-center md:p-20 py-12 px-4  ">
-      <div className="flex w-full justify-center items-center ">
-      <div className="flex mf:flex-row flex-col items-center justify-between md:p-20 py-12 px-4">
-        <div className="flex-1 flex flex-col justify-start items-start">
-          <h1 className=" text-white text-3xl sm:text-5xl py-14 text-gradient ">
-            MEET THE TEAM
-          </h1>
-
-               
-
-  <div class="snap-x ...">
-  <div class="snap-start ...">
-  <div className=" text-white text-center text-3xl  ">
-  <div class=" box-border border-double border-blue-500 border-4 shadow-md shadow-red-800 bg-blur-md bg-white/30 opacity-100 h-auto w-auto p-4 border-4 rounded-full  hover:scale-110 transition ease-out duration-300  ...">
-  <img class="w-72 h-72 rounded-full   " src="/images/WhatsApp Image 2023-02-25 at 23.57.52.jpg " />
+    <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full bg-black lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+    <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
+      <div>
+        <p className="text-3xl sm:text-5xl text-white text-gradient py-1">
+          BLOCK THIEF TEAM
+        </p>
+      </div>
+      <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight  text-white text-gradient sm:text-4xl md:mx-auto">
+       
+ Welcome our talented team
+      </h2>
+      
     </div>
-    A.M.Kavindu</div> 
-  </div>
-  <br/>
-  
-  
-  
-  <div class="snap-start ...">
-  <div className=" text-white text-center text-3xl  ">
-  <div class="box-border border-double border-blue-500 border-4 shadow-md shadow-red-800  bg-blur-md bg-white/30 opacity-100 h-auto w-auto p-4 border-4 rounded-full hover:scale-110 transition ease-out duration-300 ...">
-  <img class="w-72 h-72  rounded-full" src="/images/WhatsApp Image 2023-02-26 at 00.30.45.jpg" />
-    </div>
-    S.M.T.B. Samarakone</div>
-  </div>
-  <br/>
-
-
-  <div class="snap-start...">
-  <div className=" text-white text-center text-3xl  ">
-  <div class="box-border border-double border-blue-500 border-4  shadow-md shadow-red-800 bg-blur-md bg-white/30 opacity-100 h-auto w-auto p-4 border-4 rounded-full hover:scale-110 transition ease-out duration-300 ...">
-  <img class="w-72 h-72 rounded-full" src="/images/WhatsApp Image 2023-02-26 at 00.30.46.jpg" />
-    
-   </div> 
-   H.M.H.T Herath</div>
- </div>
- <br/>
-
- <div class="snap-start ...">
-  <div className=" text-white text-center text-3xl  ">
-  <div class=" box-border border-double border-4 border-blue-500  shadow-md shadow-red-800 bg-blur-md bg-white/30 opacity-100 h-auto w-auto p-4 border-4 rounded-full hover:scale-110 transition ease-out duration-300  ...">
-  <img class="w-72 h-72  rounded-full  " src="/images/20230211_144942.jpg" />
-    </div>
-    M.L Maitipe</div>
-</div>
-<br/>
-
-<div class="snap-start ...">
-  <div className=" text-white text-center text-3xl  ">
-  <div class=" box-border border-double border-4  border-blue-500 shadow-md shadow-red-800 bg-blur-md bg-white/30 opacity-100 h-auto w-auto p-4 border-4 rounded-full hover:scale-110 transition ease-out duration-300  ...">
-  <img class="w-72 h-72 rounded-full justify-center" src="/images/329792433_756959469187559_6251490281969289728_n.jpg" />
-   </div> 
-   K.M.H.K.Kumarasinghe</div>
-  </div>
-  <br/>
-
- </div>
- </div>
- </div>
- </div>
- </div>
-
+    <div className="grid gap-10 sm:grid-cols-2 lg:grid-cols-3">
+      <div>
+        <div className="relative overflow-hidden transition duration-300 transform rounded shadow-lg lg:hover:-translate-y-2 hover:shadow-2xl">
+          <img
+            className="object-cover w-full h-56 md:h-64 xl:h-80"
+            src="/images/WhatsApp Image 2023-02-25 at 23.57.52.jpg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260"
+            alt="Person"
+          />
+          <div className="absolute inset-0 flex flex-col justify-center px-5 py-4 text-center transition-opacity duration-300 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
+            <p className="mb-1 text-lg font-bold text-gray-100">
+            A.M. Kavindu pramodya abeysundara
+            </p>
+            <p className="mb-4 text-xs text-gray-100">Product Manager</p>
+            <p className="mb-4 text-xs tracking-wide text-gray-400">
+              Vincent Van Gogh’s most popular painting, The Starry Night.
+            </p>
+            <div className="flex items-center justify-center space-x-3">
+              <a
+                href="/"
+                className="text-white transition-colors duration-300 hover:text-teal-accent-400"
+              >
+                <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
+                  <path d="M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z" />
+                </svg>
+              </a>
+              <a
+                href="https://www.facebook.com/kavindu.pramod.92?mibextid=ZbWKwL/"
+                className="text-white transition-colors duration-300 hover:text-teal-accent-400"
+              >
+                <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
+                  <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="relative overflow-hidden transition duration-300 transform rounded shadow-lg lg:hover:-translate-y-2 hover:shadow-2xl">
+          <img
+            className="object-cover w-full h-56 md:h-64 xl:h-80"
+            src="/images/WhatsApp Image 2023-02-26 at 00.30.45.jpg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+            alt="Person"
+          />
+          <div className="absolute inset-0 flex flex-col justify-center px-5 py-4 text-center transition-opacity duration-300 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
+            <p className="mb-1 text-lg font-bold text-gray-100">
+            S.M.T.B. Samarakone
+            </p>
+            <p className="mb-4 text-xs text-gray-100">Design Team Lead</p>
+            <p className="mb-4 text-xs tracking-wide text-gray-400">
+              Amet I love liquorice jujubes pudding croissant I love pudding.
+            </p>
+            <div className="flex items-center justify-center space-x-3">
+              <a
+                href="/"
+                className="text-white transition-colors duration-300 hover:text-teal-accent-400"
+              >
+                <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
+                  <path d="M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z" />
+                </svg>
+              </a>
+              <a
+                href="/"
+                className="text-white transition-colors duration-300 hover:text-teal-accent-400"
+              >
+                <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
+                  <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="relative overflow-hidden transition duration-300 transform rounded shadow-lg lg:hover:-translate-y-2 hover:shadow-2xl">
+          <img
+            className="object-cover w-full h-56 md:h-64 xl:h-80"
+            src="/images/WhatsApp Image 2023-02-26 at 00.30.46.jpg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+            alt="Person"
+          />
+          <div className="absolute inset-0 flex flex-col justify-center px-5 py-4 text-center transition-opacity duration-300 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
+            <p className="mb-1 text-lg font-bold text-gray-100">
+            H.M.H.T Herath
+            </p>
+            <p className="mb-4 text-xs text-gray-100">CTO, Lorem Inc.</p>
+            <p className="mb-4 text-xs tracking-wide text-gray-400">
+              Apple pie macaroon toffee jujubes pie tart cookie caramels.
+            </p>
+            <div className="flex items-center justify-center space-x-3">
+              <a
+                href="/"
+                className="text-white transition-colors duration-300 hover:text-teal-accent-400"
+              >
+                <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
+                  <path d="M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z" />
+                </svg>
+              </a>
+              <a
+                href="/"
+                className="text-white transition-colors duration-300 hover:text-teal-accent-400"
+              >
+                <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
+                  <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
+      <br/>
+      <div className="grid gap-10 sm:grid-cols-2 lg:grid-cols-2">
+      <div>
+        <div className="relative overflow-hidden transition duration-300 transform rounded shadow-lg lg:hover:-translate-y-2 hover:shadow-2xl">
+          <img
+            className="object-cover w-full h-56 md:h-64 xl:h-80"
+            src="/images/329792433_756959469187559_6251490281969289728_n.jpg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+            alt="Person"
+          />
+          <div className="absolute inset-0 flex flex-col justify-center px-5 py-4 text-center transition-opacity duration-300 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
+            <p className="mb-1 text-lg font-bold text-gray-100">
+            K.M.H.K.Kumarasinghe
+            </p>
+            <p className="mb-4 text-xs text-gray-100">Human Resources</p>
+            <p className="mb-4 text-xs tracking-wide text-gray-400">
+              Lorizzle ipsum bling bling sit amizzle, consectetuer adipiscing
+              elit.
+            </p>
+            <div className="flex items-center justify-center space-x-3">
+              <a
+                href="/"
+                className="text-white transition-colors duration-300 hover:text-teal-accent-400"
+              >
+                <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
+                  <path d="M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z" />
+                </svg>
+              </a>
+              <a
+                href="https://www.facebook.com/HasiniKumarasighe?mibextid=ZbWKwL/"
+                className="text-white transition-colors duration-300 hover:text-teal-accent-400"
+              >
+                <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
+                  <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="relative overflow-hidden transition duration-300 transform rounded shadow-lg lg:hover:-translate-y-2 hover:shadow-2xl">
+          <img
+            className="object-cover w-full h-56 md:h-64 xl:h-80"
+            src="/images/20230211_144942.jpg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;w=500"
+            alt="Person"
+          />
+          <div className="absolute inset-0 flex flex-col justify-center px-5 py-4 text-center transition-opacity duration-300 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
+            <p className="mb-1 text-lg font-bold text-gray-100">
+             M.L Maitipe
+            </p>
+            <p className="mb-4 text-xs text-gray-100">Good guy</p>
+            <p className="mb-4 text-xs tracking-wide text-gray-400">
+              Bacon ipsum dolor sit amet salami jowl corned beef, andouille
+              flank.
+            </p>
+            <div className="flex items-center justify-center space-x-3">
+              <a
+                href="/"
+                className="text-white transition-colors duration-300 hover:text-teal-accent-400"
+              >
+                <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
+                  <path d="M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z" />
+                </svg>
+              </a>
+              <a
+                href="https://www.facebook.com/profile.php?id=100008736279744&mibextid=ZbWKwL/"
+                className="text-white transition-colors duration-300 hover:text-teal-accent-400"
+              >
+                <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
+                  <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        </div>
+        </div>
+        </div>
 
       
      )
