70be6bf9f19d6e2a949caa953c3a57d9c015d324
MINDIYA <mindiyamaitipe@gmail.com>
2023-04-06 08:30:56 +0530

    colour-change

--- client/src/pages/manufacturer/manuacturer.jsx
+++ client/src/pages/manufacturer/manuacturer.jsx
@@ -5,7 +5,7 @@ function manuacturer() {
   return (
     <div className="nav-spacing">
     <div class=" divide-y">
-    <div className="px-4 py-16 mx-auto bg-[url('/images/hghg.jpg')] bg-cover   sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+    <div className="px-4 py-16 mx-auto bg-gradient-to-r from-blue-600 via-cyan-500 to-teal-500 bg-cover   sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
     <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
       <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-white text-gradient  sm:text-4xl md:mx-auto">
         Secure and Authenticate Your Products with Blockchain Technology
@@ -19,7 +19,7 @@ function manuacturer() {
       <h2 className="text-base text-center text-gray-100 md:text-lg">Join our innovative platform to protect your brand and build customer trust</h2><br/>
       <div className="text-center">
         <a
-          href="/"
+          href="/registration1"
           className="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md md:w-auto bg-blue-700 hover:bg-blue-700 focus:shadow-outline focus:outline-none"
         >
           Join Now
--- client/src/pages/review.jsx
+++ client/src/pages/review.jsx
@@ -3,11 +3,11 @@ import React from "react";
 function review() {
   return (
     <div className="nav-spacing">
-    <div className="px-4 py-16 bg-black mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+    <div className="px-4 py-16 bg-gradient-to-r from-blue-600 via-cyan-500 to-teal-500 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
     <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
       <div>
         <p className="text-3xl sm:text-5xl text-white text-gradient py-1">
-          BLOCK THIEF TEAM
+          Reviews
         </p>
       </div>
       </div>
